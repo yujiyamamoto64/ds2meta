2acc51e2a28509d011bd92b3929c35f925410b17
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-09-06 12:06:37 -0300

    project created

---
+++ backend/src/test/java/com/yamamoto64/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamamoto64.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
